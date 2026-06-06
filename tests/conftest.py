2dc32e34431813a52ee2b59c9c60f248bbcbedcf
Tim Abdiukov <TAbdiukov@gmail.com>
2026-06-06 18:10:11 +0200

    Remove argument no longer used

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-def pytest_addoption(parser):
-    parser.addoption(
-        "--check-functionality",
-        "--check-func",
-        action="store_true",
-        default=False,
-        help="Actually connect to a server and run tests.",
-    )
